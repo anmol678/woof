ae0a89b7568b8cefbdef1e76401dcc70b0e989d9
Anmol Jain <anmolj678@gmail.com>
2024-09-26 18:37:26 -0700

    update account service

--- api/app/services/account.py
+++ api/app/services/account.py
@@ -5,7 +5,8 @@ from sqlalchemy.future import select
 
 from app.models import Account, ACCOUNT_NUMBER_LENGTH, Customer
 from app.schemas import AccountCreate
-from app.exceptions import CustomerNotFoundException, AccountNotFoundException
+from app.exceptions import *
+from app.utils.validators import validate_customer_number,validate_account_number
 
 
 class AccountService:
@@ -13,12 +14,17 @@ class AccountService:
 
     @staticmethod
     async def create_account(account: AccountCreate, db: AsyncSession) -> Account:
-         async with db.begin():
-            result = await db.execute(select(Customer).where(Customer.id == account.customer_id).with_for_update())
+        if not validate_customer_number(account.customer_number):
+            raise InvalidCustomerNumberException(customer_number=account.customer_number)
+        async with db.begin():
+            result = await db.execute(
+                select(Customer)
+                .where(Customer.customer_number == account.customer_number)
+                .with_for_update()
+            )
             customer = result.scalar_one_or_none()
             if not customer:
-                raise CustomerNotFoundException(customer_id=account.customer_id)
-            
+                raise CustomerNotFoundException(customer_number=account.customer_number)
             account_number = await AccountService.generate_unique_account_number(db)
             db_account = Account(
                 account_number=account_number,
@@ -30,11 +36,13 @@ class AccountService:
             return db_account
 
     @staticmethod
-    async def get_account(account_id: int, db: AsyncSession) -> Account:
-        result = await db.execute(select(Account).where(Account.id == account_id))
+    async def get_account(account_number: str, db: AsyncSession) -> Account:
+        if not validate_account_number(account_number):
+            raise InvalidAccountNumberException(account_number=account_number)
+        result = await db.execute(select(Account).where(Account.account_number == account_number))
         account = result.scalar_one_or_none()
         if not account:
-            raise AccountNotFoundException(account_id=account_id)
+            raise AccountNotFoundException(account_number=account_number)
         return account
 
     @staticmethod
@@ -45,9 +53,7 @@ class AccountService:
                 result = await db.execute(
                     select(Account)
                     .where(Account.account_number == account_number)
-                    .with_for_update()
                     .limit(1)
                 )
-                existing_account = result.scalar_one_or_none()
-                if not existing_account:
+                if result.first() is None:
                     return account_number
