dd08ba786019e9b8be56fea1acb12ef0ef90693f
Anmol Jain <anmolj678@gmail.com>
2024-09-26 17:53:49 -0700

    add routers

--- api/app/main.py
+++ api/app/main.py
@@ -2,6 +2,7 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.routes import customers, accounts, transfers
 from app.db import init_db
 from app.utils.logger import logger
 
@@ -14,6 +15,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+base_prefix = "/api/v1"
+
+app.include_router(customers.router, prefix=base_prefix + "/customers", tags=["Customers"])
+app.include_router(accounts.router, prefix=base_prefix + "/accounts", tags=["Accounts"])
+app.include_router(transfers.router, prefix=base_prefix + "/transfers", tags=["Transfers"])
 
 @app.on_event("startup")
 async def startup_event():
