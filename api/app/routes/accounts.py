79fd4177845ff9a10aea5ea0b230ed403fc00e48
Anmol Jain <anmolj678@gmail.com>
2024-09-26 17:55:23 -0700

    add acc transfers

--- api/app/routes/accounts.py
+++ api/app/routes/accounts.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db import get_db
-from app.schemas import AccountCreate, AccountBalance, Account
-from app.services import AccountService
+from app.schemas import AccountCreate, AccountBalance, Account, Transfer
+from app.services import AccountService, TransferService
 
 
 router = APIRouter()
@@ -20,3 +20,7 @@ async def get_balance(account_id: int, db: AsyncSession = Depends(get_db)):
 @router.get("/{account_id}", response_model=Account, status_code=200)
 async def get_account(account_id: int, db: AsyncSession = Depends(get_db)):
     return await AccountService.get_account(account_id, db)
+
+@router.get("/{account_id}/transfers/", response_model=list[Transfer], status_code=200)
+async def get_transfer_history(account_id: int, db: AsyncSession = Depends(get_db)):
+    return await TransferService.get_transfer_history(account_id, db)
---
+++ api/app/services/__init__.py
@@ -0,0 +1,9 @@
+from .account import AccountService
+from .customer import CustomerService
+from .transfer import TransferService
+
+__all__ = [
+    "AccountService",
+    "CustomerService",
+    "TransferService"
+]
