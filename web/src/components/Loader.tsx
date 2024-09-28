a80b8b5884283fbf0eef9b5b5e4869a785af01b0
Anmol Jain <anmolj678@gmail.com>
2024-09-28 13:57:45 -0700

    loader styles

--- web/src/components/Loader.tsx
+++ web/src/components/Loader.tsx
@@ -1,23 +1,14 @@
 import { cn } from '@/utils/styles'
 
-export default function Loader({ className }: { className?: string }) {
+interface LoaderProps {
+  className?: string
+  'data-style'?: 'accent' | null
+}
+
+export default function Loader({ className, 'data-style': style }: LoaderProps) {
   return (
     <div className={cn('flex items-center justify-center', className)}>
-      <div className="loader h-5 w-5 rounded-full border-2 border-t-2 border-gray-200 ease-linear"></div>
-      <style jsx>{`
-        .loader {
-          border-top-color: transparent;
-          animation: spin 1s linear infinite;
-        }
-        @keyframes spin {
-          0% {
-            transform: rotate(0deg);
-          }
-          100% {
-            transform: rotate(360deg);
-          }
-        }
-      `}</style>
+      <div className="loader" data-style={style}></div>
     </div>
   )
 }
