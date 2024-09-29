40e2904b37f65cb5d2389bb71aad6871fb32f7fb
Anmol Jain <anmolj678@gmail.com>
2024-09-29 01:06:08 -0700

    create params

---
+++ web/src/utils/params.ts
@@ -0,0 +1,9 @@
+enum Params {
+  CUSTOMER_NUMBER = 'customerNumber',
+  ACCOUNT_NUMBER = 'accountNumber',
+  TRANSFER_FROM = 'transferFrom',
+  TRANSFER_TO = 'transferTo',
+  FROM = 'from'
+}
+
+export default Params
