5d40a0c0e477a1a9e030d4976c95ada342980f1a
Anmol Jain <anmolj678@gmail.com>
2024-09-28 17:49:27 -0700

    add transfer path

--- web/src/utils/paths.ts
+++ web/src/utils/paths.ts
@@ -2,7 +2,8 @@ enum PATHS {
   CREATE_ACCOUNT = '/accounts/create',
   CREATE_CUSTOMER = '/customers/create',
   CUSTOMER_DETAILS = '/customers',
-  ACCOUNT_DETAILS = '/accounts'
+  ACCOUNT_DETAILS = '/accounts',
+  TRANSFER = '/transfer'
 }
 
 export default PATHS
